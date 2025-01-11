d022d51175561f04eeb3dce6450ffe2f691bf62c
AshishAgam <ashishagam6@gmail.com>
2025-01-11 15:06:18 +0530

    change app.js file

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Namaste India
+          Welcome to react app
         </p>
         <a
           className="App-link"
